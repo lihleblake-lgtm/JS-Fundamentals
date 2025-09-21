ce1091fc8ee7d58b3ae345e947e6b5baf3e68be2
Thubelihle Bianca <lihleblake@gmail.com>
2025-09-21 16:42:57 -0700

    added 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const arg = process.argv[2];
+const count = parseInt(arg);
+
+if (isNaN(count) || count <= 0) {
+console.log("Missing number of occurrences");
+} else {
+    let i = 0;
+    while (i < count) {
+        console.log("C is fun");
+        i++;
+    }
+}
