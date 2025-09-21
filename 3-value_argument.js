ed3bbb4197b5b4509eae51068a2e39ee1541478b
Thubelihle Bianca <lihleblake@gmail.com>
2025-09-21 15:31:34 -0700

    added 4-concat.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,4 +1,4 @@
-const arg = ProcessingInstruction.argv[2];
+const arg = process.argv[2];
 
 if (arg === undefined) {
     console.log("No argument");
---
+++ 4-concat.js
@@ -0,0 +1,4 @@
+const arg1 = Process.argv[2];
+const arg2 = Process.argv[3];
+
+console.log(arg1 + " is " + arg2);
